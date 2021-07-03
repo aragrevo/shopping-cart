5ed892feb75d7a20c38ebbf941375b28b85b494e
aragrevo <eduver_san@hotmail.com>
2021-07-03 12:39:24 -0500

    main pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './theme/layout/auth/auth.component';
+import { ShoppingComponent } from './theme/layout/shopping/shopping.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  // {
+  //   path: '',
+  //   component: AuthComponent,
+  //   children: [
+
+  //     // {
+  //     //   path: 'auth',
+  //     //   loadChildren: () => import('./views/authentication/authentication.module').then(module => module.AuthenticationModule)
+  //     // },
+  //   ]
+  // },
+  {
+    path: '',
+    component: ShoppingComponent,
+    children: [
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      {
+        path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)
+      }
+    ]
+  }
+
 ];
 
 @NgModule({
